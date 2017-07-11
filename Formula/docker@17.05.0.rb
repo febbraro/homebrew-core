ac3624713532655463949cd0e2fb709be7fbc363
Frank Febbraro <frank@phase2technology.com>
2017-07-11 12:16:02 -0700

    Adding previous version of Docker

---
+++ Formula/docker@17.05.0.rb
@@ -0,0 +1,48 @@
+class Docker < Formula
+  desc "Pack, ship and run any application as a lightweight container"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/docker-ce.git",
+      :tag => "v17.05.0-ce",
+      :revision => "89658bed64c2a8fe05a978e5b87dbec409d57a0f"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d60ad4d00efd0d0358a93fe19d91ce155a37a184e4a98b29e74a215d17497b26" => :sierra
+    sha256 "7faaea79928b474f1f01fcca3864fa1effe49c9f3519279555f88e97310d5852" => :el_capitan
+    sha256 "7f9827b8ff928e62e49a76a1d2a3d35a3c5b2b2319db9d83f560742415700dd4" => :yosemite
+  end
+
+  option "with-experimental", "Enable experimental features"
+  option "without-completions", "Disable bash/zsh completions"
+
+  depends_on "go" => :build
+
+  if build.with? "experimental"
+    depends_on "libtool" => :run
+    depends_on "yubico-piv-tool" => :recommended
+  end
+
+  def install
+    ENV["DOCKER_EXPERIMENTAL"] = "1" if build.with? "experimental"
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/docker/cli"
+    dir.install (buildpath/"components/cli").children
+    cd dir do
+      commit = Utils.popen_read("git rev-parse --short HEAD").chomp
+      ldflags = ["-X github.com/docker/cli/cli.GitCommit=#{commit}",
+                 "-X github.com/docker/cli/cli.Version=#{version}-ce"]
+      system "go", "build", "-o", bin/"docker", "-ldflags", ldflags.join(" "),
+             "github.com/docker/cli/cmd/docker"
+
+      if build.with? "completions"
+        bash_completion.install "contrib/completion/bash/docker"
+        fish_completion.install "contrib/completion/fish/docker.fish"
+        zsh_completion.install "contrib/completion/zsh/_docker"
+      end
+    end
+  end
+
+  test do
+    system "#{bin}/docker", "--version"
+  end
+end
